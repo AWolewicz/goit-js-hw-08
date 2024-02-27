b201f28808253d58b89b5535de49012a74894b5d
AWolewicz <agnieszkawolewicz5@gmail.com>
2024-02-27 10:35:28 +0100

    homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,36 @@
 import Player from '@vimeo/player';
-// import lodash?
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
     const player = new Player(iframe);
 
-    player.on('play',() => {
-        console.log('played the video!');
-    });
+const onPlay = data => {
+    try {
+    const currentTime = JSON.stringify(data.seconds);
+    localStorage.setItem('videoplayer-current-time', currentTime);
+    } catch (error) {
+    console.log(error);
+    }
+};
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+const throttlePlay = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttlePlay);
+
+try {
+    player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+    );
+} catch (error) {
+    switch (error.name) {
+    case 'RangeError':
+    console.log(
+        'The time was less than 0 or greater than the video’s duration'
+    );
+    break;
+
+    default:
+    console.log('An error occured');
+    break;
+    }
+}
